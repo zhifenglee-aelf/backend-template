b40ef8a5dee2b8ea3822411f83fef319e34dfa83
zhifenglee-aelf <zhifeng.lee@aelf.io>
2024-07-03 00:17:38 +0800

    feat(silo): fixed naming

--- src/MyTemplate.Silo/SiloModule.cs
+++ src/MyTemplate.Silo/SiloModule.cs
@@ -1,24 +1,22 @@
 using MyTemplate.Domain.Grains;
-using MyTemplate.MongoDB;
 using Microsoft.Extensions.DependencyInjection;
+using MyTemplate.Application.Grains;
 using Volo.Abp.AspNetCore.Serilog;
 using Volo.Abp.Autofac;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
-namespace AbpOrleansAppTemplate.Silo;
+namespace MyTemplate.Silo;
 
 [DependsOn(
-    typeof(MyTemplateDomainGrainsModule),
     typeof(AbpAspNetCoreSerilogModule),
-    //typeof(AbpOrleansAppTemplateMongoDbModule),
     typeof(AbpAutofacModule)
 )]
-public class MyTemplateOrleansSiloModule : AbpModule
+public class SiloModule : AbpModule, IDomainGrainsModule, IApplicationGrainsModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        Configure<AbpAutoMapperOptions>(options => { options.AddMaps<MyTemplateOrleansSiloModule>(); });
-        context.Services.AddHostedService<AbpOrleansBackendTemplateHostedService>();
+        Configure<AbpAutoMapperOptions>(options => { options.AddMaps<SiloModule>(); });
+        context.Services.AddHostedService<MyTemplateHostedService>();
         var configuration = context.Services.GetConfiguration();
         //add dependencies here
 
